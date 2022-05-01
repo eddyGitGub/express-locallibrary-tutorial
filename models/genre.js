197e9e1de0a15eb2f70e215d05c7d45891ba66b2
eddygitgub <87383463+EdwardAnoghena@users.noreply.github.com>
2022-05-02 00:15:48 +0100

    initiated and model addes

---
+++ models/genre.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        url: { type: String }
+    }
+);
+
+//Export model
+module.exports = mongoose.model('Genre', GenreSchema);
